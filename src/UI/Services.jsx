f89fedfc6c818a5eb72a3d7b28d5d462622a67d4
Mohsin Riaz <mohsin.riaz338@gmail.com>
2024-11-15 07:34:31 +0500

    initial commit

---
+++ src/UI/Services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OurServices from "./OurServices";
+
+const Services = () => {
+  return (
+    <div>
+      <p className="px-36 py-3 text-3xl font-bold text-primary">Our Services</p>
+      <OurServices />
+    </div>
+  );
+};
+
+export default Services;
